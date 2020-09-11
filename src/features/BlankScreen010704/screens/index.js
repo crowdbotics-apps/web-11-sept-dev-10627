313037c1c1d4349674a4c784b064321c33d608d5
Crowdbotics <team@crowdbotics.com>
2020-09-11 15:51:57 +0000

    Update components

--- src/features/BlankScreen010704/screens/index.js
+++ src/features/BlankScreen010704/screens/index.js
@@ -22,10 +22,9 @@ export class __Blank extends React.Component {
   state = {
     Toggle_5: true,
     Toggle_7: true,
-    Input_11: "",
     CheckBox_13: true,
-    Toggle_48: true,
-    Toggle_44: true
+    Toggle_44: true,
+    Toggle_48: true
   }
   render = () => (
     <View
@@ -185,50 +184,6 @@ export class __Blank extends React.Component {
           borderRadius: 0
         }}
       />
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        multiline={true}
-        textStyle={{
-          height: 100,
-          fontSize: 12,
-          color: "#000000",
-          textAlign: "left",
-          fontFamily: "Roboto-Regular"
-        }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textAlign: "left",
-          textAlignVertical: "center",
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        value={this.state.Input_11}
-        onChangeText={nextValue => this.setState({ Input_11: nextValue })}
-      />
       <CheckBox
         text="Checkbox"
         disabled={false}
@@ -259,39 +214,6 @@ export class __Blank extends React.Component {
         }}
         checked={this.state.CheckBox_13}
         onChange={nextChecked => this.setState({ CheckBox_13: nextChecked })}
-      />
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          alignContent: "stretch",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          backgroundSize: "auto"
-        }}
       />
       <Icon
         iconFont="Eva Design Icons"
@@ -328,240 +250,6 @@ export class __Blank extends React.Component {
           letterSpacing: 0
         }}
       />
-      <View
-        style={{
-          width: "100%",
-          height: "11.11111111111111%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          alignContent: "stretch",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          backgroundSize: "auto"
-        }}
-      >
-        <View
-          style={{
-            width: "100%",
-            height: "50%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontFamily: "Roboto-Regular",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            width: "100%",
-            height: "50%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontFamily: "Roboto-Regular",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        />
-      </View>
-      <View
-        style={{
-          width: "100%",
-          height: "10%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          alignContent: "stretch",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          backgroundSize: "auto"
-        }}
-      >
-        <View
-          style={{
-            width: "50%",
-            height: "100%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontFamily: "Roboto-Regular",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        >
-          <Toggle
-            text="switch ON/OFF"
-            activeColor="#409EFF"
-            inactiveColor="#C0CCDA"
-            disabled={false}
-            style={{
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textAlign: "left",
-              textAlignVertical: "center",
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            checked={this.state.Toggle_48}
-            onChange={nextChecked => this.setState({ Toggle_48: nextChecked })}
-          />
-        </View>
-        <View
-          style={{
-            width: "50%",
-            height: "100%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontFamily: "Roboto-Regular",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        />
-      </View>
       <Button
         textStyle={{
           fontSize: 12,
@@ -635,6 +323,97 @@ export class __Blank extends React.Component {
         }}
         checked={this.state.Toggle_44}
         onChange={nextChecked => this.setState({ Toggle_44: nextChecked })}
+      />
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        disabled={false}
+        maximumTrackTintColor="#E4E7ED"
+        minimumTrackTintColor="#3366FF"
+        thumbTintColor="#3366FF"
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
+      />
+      <Toggle
+        text="switch ON/OFF"
+        activeColor="#409EFF"
+        inactiveColor="#C0CCDA"
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+        checked={this.state.Toggle_48}
+        onChange={nextChecked => this.setState({ Toggle_48: nextChecked })}
+      />
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        disabled={false}
+        maximumTrackTintColor="#E4E7ED"
+        minimumTrackTintColor="#3366FF"
+        thumbTintColor="#3366FF"
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
       />
       <Slider
         value={50}
